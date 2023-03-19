f1b2d415076ce497b317bacedf29bd7bd6fd483d
nfallis96 <nfallis2019@fau.edu>
2023-03-18 20:21:43 -0400

    finished server.js first file

---
+++ server.js
@@ -0,0 +1,31 @@
+// required express & mongoose
+const express = require('express');
+const mongoose = require('mongoose');
+const PORT = process.env.PORT || 3001;
+const app = express();
+
+
+app.use(express.json());
+
+app.use(express.urlencoded({ extended: true }));
+
+app.use(express.static('public'));
+
+app.use(require('./routes'));
+
+// for connecting to mongoose
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/social-network-api', {
+    useNewURLParser: true,
+    useUnifiedTopology: true
+});
+
+// listener-queries and use this log to execute queries
+mongoose.set('debug', true);
+
+app.listen(PORT, () => {
+
+        console.log(`API server running on port ${PORT}!`);
+      });
+
+
+    
